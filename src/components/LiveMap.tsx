93e1bd40ffac79d1e8a987c3d4aae0bd84f762d0
pritam1991verma <pritam@bdph.in>
2026-05-26 23:59:39 +0530

    Update LiveMap.tsx

--- src/components/LiveMap.tsx
+++ src/components/LiveMap.tsx
@@ -48,12 +48,17 @@ const yellowIcon = divIcon({
 });
 
 function LiveMap() {
+
   useEffect(() => {
     L.Icon.Default.mergeOptions({});
   }, []);
 
   return (
-    <section id="tracking" className="py-24 px-6 bg-black">
+
+    <section
+      id="tracking"
+      className="py-24 px-6 bg-black overflow-hidden max-w-full"
+    >
 
       <div className="max-w-7xl mx-auto">
 
@@ -79,136 +84,137 @@ function LiveMap() {
             center={[22.8046, 86.2029]}
             zoom={11}
             scrollWheelZoom={true}
-            className="h-[750px] w-full z-0"
+            className="h-[750px] w-full max-w-full overflow-hidden z-0"
           >
 
             {/* HD SATELLITE */}
 
             <TileLayer
-              attribution='&copy; Esri'
+              attribution="&copy; Esri"
               url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             />
-{/* VEHICLE NETWORK */}
 
-<Marker position={[22.8046, 86.2029]} icon={greenIcon}>
-  <Popup>BDPH-1021 • ACTIVE</Popup>
-</Marker>
+            {/* VEHICLE NETWORK */}
 
-<Marker position={[22.8246, 86.1829]} icon={redIcon}>
-  <Popup>BDPH-2044 • FUEL ALERT</Popup>
-</Marker>
+            <Marker position={[22.8046, 86.2029]} icon={greenIcon}>
+              <Popup>BDPH-1021 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.7846, 86.2229]} icon={yellowIcon}>
-  <Popup>BDPH-3301 • BATTERY WARNING</Popup>
-</Marker>
+            <Marker position={[22.8246, 86.1829]} icon={redIcon}>
+              <Popup>BDPH-2044 • FUEL ALERT</Popup>
+            </Marker>
 
-<Marker position={[22.8121, 86.2511]} icon={greenIcon}>
-  <Popup>BDPH-4410 • ROUTE ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.7846, 86.2229]} icon={yellowIcon}>
+              <Popup>BDPH-3301 • BATTERY WARNING</Popup>
+            </Marker>
 
-<Marker position={[22.7721, 86.1911]} icon={greenIcon}>
-  <Popup>BDPH-5502 • DELIVERY ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.8121, 86.2511]} icon={greenIcon}>
+              <Popup>BDPH-4410 • ROUTE ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.8421, 86.2311]} icon={redIcon}>
-  <Popup>BDPH-6619 • ENGINE ALERT</Popup>
-</Marker>
+            <Marker position={[22.7721, 86.1911]} icon={greenIcon}>
+              <Popup>BDPH-5502 • DELIVERY ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.7921, 86.2711]} icon={yellowIcon}>
-  <Popup>BDPH-7720 • LOW BATTERY</Popup>
-</Marker>
+            <Marker position={[22.8421, 86.2311]} icon={redIcon}>
+              <Popup>BDPH-6619 • ENGINE ALERT</Popup>
+            </Marker>
 
-<Marker position={[22.7521, 86.2411]} icon={greenIcon}>
-  <Popup>BDPH-8101 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.7921, 86.2711]} icon={yellowIcon}>
+              <Popup>BDPH-7720 • LOW BATTERY</Popup>
+            </Marker>
 
-<Marker position={[22.8621, 86.1711]} icon={greenIcon}>
-  <Popup>BDPH-9104 • RUNNING</Popup>
-</Marker>
+            <Marker position={[22.7521, 86.2411]} icon={greenIcon}>
+              <Popup>BDPH-8101 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.8721, 86.2811]} icon={yellowIcon}>
-  <Popup>BDPH-1130 • MONITORING</Popup>
-</Marker>
+            <Marker position={[22.8621, 86.1711]} icon={greenIcon}>
+              <Popup>BDPH-9104 • RUNNING</Popup>
+            </Marker>
 
-<Marker position={[22.7421, 86.1411]} icon={greenIcon}>
-  <Popup>BDPH-1208 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.8721, 86.2811]} icon={yellowIcon}>
+              <Popup>BDPH-1130 • MONITORING</Popup>
+            </Marker>
 
-<Marker position={[22.7221, 86.3011]} icon={redIcon}>
-  <Popup>BDPH-1304 • SOS ALERT</Popup>
-</Marker>
+            <Marker position={[22.7421, 86.1411]} icon={greenIcon}>
+              <Popup>BDPH-1208 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.8921, 86.2111]} icon={greenIcon}>
-  <Popup>BDPH-1420 • ONLINE</Popup>
-</Marker>
+            <Marker position={[22.7221, 86.3011]} icon={redIcon}>
+              <Popup>BDPH-1304 • SOS ALERT</Popup>
+            </Marker>
 
-<Marker position={[22.9121, 86.2411]} icon={yellowIcon}>
-  <Popup>BDPH-1520 • BATTERY CHECK</Popup>
-</Marker>
+            <Marker position={[22.8921, 86.2111]} icon={greenIcon}>
+              <Popup>BDPH-1420 • ONLINE</Popup>
+            </Marker>
 
-<Marker position={[22.6921, 86.2611]} icon={greenIcon}>
-  <Popup>BDPH-1601 • DELIVERY ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.9121, 86.2411]} icon={yellowIcon}>
+              <Popup>BDPH-1520 • BATTERY CHECK</Popup>
+            </Marker>
 
-<Marker position={[22.6621, 86.2211]} icon={greenIcon}>
-  <Popup>BDPH-1722 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.6921, 86.2611]} icon={greenIcon}>
+              <Popup>BDPH-1601 • DELIVERY ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.6521, 86.1611]} icon={redIcon}>
-  <Popup>BDPH-1820 • FUEL CRITICAL</Popup>
-</Marker>
+            <Marker position={[22.6621, 86.2211]} icon={greenIcon}>
+              <Popup>BDPH-1722 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.9421, 86.1911]} icon={greenIcon}>
-  <Popup>BDPH-1930 • TRACKING LIVE</Popup>
-</Marker>
+            <Marker position={[22.6521, 86.1611]} icon={redIcon}>
+              <Popup>BDPH-1820 • FUEL CRITICAL</Popup>
+            </Marker>
 
-<Marker position={[22.9521, 86.2911]} icon={yellowIcon}>
-  <Popup>BDPH-2001 • OBSERVATION</Popup>
-</Marker>
+            <Marker position={[22.9421, 86.1911]} icon={greenIcon}>
+              <Popup>BDPH-1930 • TRACKING LIVE</Popup>
+            </Marker>
 
-<Marker position={[22.9821, 86.3211]} icon={greenIcon}>
-  <Popup>BDPH-2109 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.9521, 86.2911]} icon={yellowIcon}>
+              <Popup>BDPH-2001 • OBSERVATION</Popup>
+            </Marker>
 
-<Marker position={[22.6321, 86.1111]} icon={greenIcon}>
-  <Popup>BDPH-2210 • ONLINE</Popup>
-</Marker>
+            <Marker position={[22.9821, 86.3211]} icon={greenIcon}>
+              <Popup>BDPH-2109 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.6121, 86.3411]} icon={redIcon}>
-  <Popup>BDPH-2301 • SECURITY ALERT</Popup>
-</Marker>
+            <Marker position={[22.6321, 86.1111]} icon={greenIcon}>
+              <Popup>BDPH-2210 • ONLINE</Popup>
+            </Marker>
 
-<Marker position={[22.9921, 86.1411]} icon={greenIcon}>
-  <Popup>BDPH-2411 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.6121, 86.3411]} icon={redIcon}>
+              <Popup>BDPH-2301 • SECURITY ALERT</Popup>
+            </Marker>
 
-<Marker position={[22.7021, 86.3611]} icon={yellowIcon}>
-  <Popup>BDPH-2511 • LOW VOLTAGE</Popup>
-</Marker>
+            <Marker position={[22.9921, 86.1411]} icon={greenIcon}>
+              <Popup>BDPH-2411 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.7421, 86.4011]} icon={greenIcon}>
-  <Popup>BDPH-2604 • MOVING</Popup>
-</Marker>
+            <Marker position={[22.7021, 86.3611]} icon={yellowIcon}>
+              <Popup>BDPH-2511 • LOW VOLTAGE</Popup>
+            </Marker>
 
-<Marker position={[22.8321, 86.3911]} icon={greenIcon}>
-  <Popup>BDPH-2710 • RUNNING</Popup>
-</Marker>
+            <Marker position={[22.7421, 86.4011]} icon={greenIcon}>
+              <Popup>BDPH-2604 • MOVING</Popup>
+            </Marker>
 
-<Marker position={[22.9221, 86.3711]} icon={redIcon}>
-  <Popup>BDPH-2811 • GPS SIGNAL LOST</Popup>
-</Marker>
+            <Marker position={[22.8321, 86.3911]} icon={greenIcon}>
+              <Popup>BDPH-2710 • RUNNING</Popup>
+            </Marker>
 
-<Marker position={[22.5621, 86.1811]} icon={greenIcon}>
-  <Popup>BDPH-2910 • ACTIVE</Popup>
-</Marker>
+            <Marker position={[22.9221, 86.3711]} icon={redIcon}>
+              <Popup>BDPH-2811 • GPS SIGNAL LOST</Popup>
+            </Marker>
 
-<Marker position={[22.5421, 86.2911]} icon={yellowIcon}>
-  <Popup>BDPH-3011 • CHECK REQUIRED</Popup>
-</Marker>
+            <Marker position={[22.5621, 86.1811]} icon={greenIcon}>
+              <Popup>BDPH-2910 • ACTIVE</Popup>
+            </Marker>
 
-<Marker position={[22.5221, 86.3511]} icon={greenIcon}>
-  <Popup>BDPH-3110 • LIVE TRACKING</Popup>
-</Marker>
+            <Marker position={[22.5421, 86.2911]} icon={yellowIcon}>
+              <Popup>BDPH-3011 • CHECK REQUIRED</Popup>
+            </Marker>
+
+            <Marker position={[22.5221, 86.3511]} icon={greenIcon}>
+              <Popup>BDPH-3110 • LIVE TRACKING</Popup>
+            </Marker>
 
             {/* LIVE ZONES */}
 
@@ -239,6 +245,7 @@ function LiveMap() {
       </div>
 
     </section>
+
   );
 }
 
