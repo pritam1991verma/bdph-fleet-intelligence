7279d3d0bd1fb46cdf3ba62dc88902dc77494f27
pritam1991verma <pritam@bdph.in>
2026-05-26 18:57:50 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-black/70 backdrop-blur-md border-b border-cyan-500/20 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-black/50 backdrop-blur-md border-b border-white/10 z-50">
 
       <div className="max-w-7xl mx-auto flex justify-between items-center px-8 py-3">
 
@@ -14,7 +14,7 @@ function Navbar() {
             className="h-16 w-auto object-contain"
           />
 
-          <h1 className="hidden md:block text-2xl font-bold text-white tracking-[0.18em]">
+          <h1 className="hidden md:block text-xl font-semibold text-white tracking-[0.18em]">
             BDPH GROUP
           </h1>
 
@@ -22,11 +22,11 @@ function Navbar() {
 
         {/* MENU */}
 
-        <div className="hidden md:flex gap-10 uppercase text-sm tracking-[0.2em] font-semibold text-white">
+        <div className="hidden md:flex gap-10 uppercase text-sm tracking-[0.2em] font-semibold text-gray-200">
 
           <a
             href="#home"
-            className="hover:text-cyan-400 transition duration-300"
+            className="hover:text-cyan-400 hover:drop-shadow-[0_0_12px_rgba(34,211,238,0.8)] transition duration-300"
           >
             Home
           </a>
