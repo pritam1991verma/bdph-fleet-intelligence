5e9c981f0313ee3257596dd07b3c7d2408a1e7fe
pritam1991verma <pritam@bdph.in>
2026-06-15 02:11:14 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -63,6 +63,7 @@ function Navbar() {
   );
 
   return (
+      <>
 <div className="fixed top-0 left-0 w-full bg-[#0F4C81] text-white text-xs z-50 overflow-hidden">
 
   <div className="w-full py-2">
