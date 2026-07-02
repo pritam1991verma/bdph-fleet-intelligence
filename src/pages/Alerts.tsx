ea3535850e01640a8dee7e659fdc4f3568b9bfdd
pritam1991verma <pritam@bdph.in>
2026-07-02 23:39:49 +0530

    Update Alerts.tsx

--- src/pages/Alerts.tsx
+++ src/pages/Alerts.tsx
@@ -1,1 +1,10 @@
 
+function Alerts() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Alerts Page</h1>
+    </div>
+  );
+}
+
+export default Alerts;
